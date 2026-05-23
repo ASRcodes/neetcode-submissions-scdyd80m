bfb9aef503f7e72271f3a125d1a111c28a365997
Anubhav Singh <147413988+ASRcodes@users.noreply.github.com>
2026-05-23 14:10:51 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int helper(int[] a, int n,int[] dp){
+        if(n==0) return 0;
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0;i<a.length;i++){
+            if(n-a[i]>=0){
+                int subAns = 0;
+                if(dp[n-a[i]] != -1){
+                    subAns = dp[n-a[i]];
+                }else{
+                    subAns = helper(a,n-a[i],dp);
+                }
+                if(subAns != Integer.MAX_VALUE){
+                    ans = Math.min(ans,subAns+1);
+                }
+            }
+        }
+        return dp[n] = ans;
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp,-1);
+        dp[0] = 0;
+        int ans = helper(coins,amount,dp);
+        return ans == Integer.MAX_VALUE?-1:ans;
+    }
+}
