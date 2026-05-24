07a60db2853ef705f74d1e79db900709b09bbf14
Anubhav Singh <147413988+ASRcodes@users.noreply.github.com>
2026-05-24 16:33:09 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int prev = 1,prev2 = 1;
+        int curr = 0;
+        for(int i=2;i<=n;i++){
+            curr = prev+prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
